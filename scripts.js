165c6a352354d1e43ca245ca56ff2206a28fa2fe
Dimitri Remoundos <dimitri.remoundos@gmail.com>
2023-10-18 17:24:42 +0200

    working on mobile and desktop

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// Make the DIV element draggable:
+// Make the control panel draggable:
 dragElement(document.getElementById("control-panel"));
 
 function dragElement(elmnt) {
@@ -40,63 +40,93 @@ function dragElement(elmnt) {
   }
 }
 
-const rangeInput = document.getElementById("rotation-speed");
-const animatedElement = document.getElementById("kaleidoscope");
-const krsInd = document.getElementById("krs-indicator");
-const segmentsAmount = document.getElementById("segments-amount");
-const saInd = document.getElementById("sa-indicator");
-const particleCount = document.getElementById("particle-count");
-const pcInd = document.getElementById("pc-indicator");
+const rotationSpeedInput = document.getElementById("rotation-speed");
+const kaleidoscope = document.getElementById("kaleidoscope");
+const kaleidoSpeedIndicator = document.getElementById("krs-indicator");
+const segmentsAmountInput = document.getElementById("segments-amount");
+const segmentIndicator = document.getElementById("sa-indicator");
+const particleCountInput = document.getElementById("particle-count");
+const particleCountIndicator = document.getElementById("pc-indicator");
+const closeButton = document.getElementById("closeButton");
+const controlPanel = document.getElementById("control-panel");
+const closeNotification = document.getElementById("hideNotification");
+const mobileNotification = document.getElementById("mobileNotification");
+const widthLookUp = [
+  0, 100, 100, 174, 100, 73, 58, 48.62, 41.25, 36.66, 32.5, 29.5, 26.6,
+];
+let gemsAmount = 1;
+let segmentCount = 2;
 
-// kaleidoscope rotation control
-rangeInput.addEventListener("input", function () {
-  const durationValue = this.value;
-  animatedElement.style.animationDuration = durationValue + "s";
-  krsInd.innerText = durationValue + "s";
+closeButton.addEventListener("click", () => {
+  controlPanel.style.visibility = "hidden";
+  closeNotification.style.display = "block";
+  closeNotification.style.animationName = "fade-out";
+  setTimeout(() => (closeNotification.style.display = "none"), 3000);
 });
 
+document.addEventListener("keydown", function () {
+  if (controlPanel.style.visibility === "hidden") {
+    controlPanel.style.visibility = "visible";
+  }
+});
+
+// kaleidoscope rotation control
+
+rotationSpeedInput.addEventListener("input", function () {
+  const durationValue = this.max - (this.value - this.min);
+  kaleidoscope.style.animationDuration = durationValue + "s";
+  kaleidoSpeedIndicator.innerText = durationValue.toFixed(1) + "s";
+});
+
+// create master segment
+const segment = document.createElement("div");
+segment.classList.add("segment");
+
+particleCountInput.addEventListener("input", function () {
+  gemsAmount = this.value;
+  segment.innerHTML = "";
+  particleCountIndicator.innerText = gemsAmount;
+  createGems(gemsAmount);
+  createSegments(segmentCount);
+  mobileNotification.style.visibility = "hidden";
+});
+
+//add random "gem" particles
+function createGems() {
+  for (let i = 0; i < gemsAmount; i++) {
+    const gem = document.createElement("div");
+    gem.classList.add("gem" + (Math.floor(Math.random() * 3) + 1));
+    gem.style.borderColor =
+      "#" + Math.floor(Math.random() * 16777215).toString(16);
+    gem.style.left = Math.random() * 100 + "px";
+    gem.style.top = Math.random() * 100 + "px";
+    gem.style.animationName = "anim" + (Math.floor(Math.random() * 3) + 1);
+    gem.className === "gem3"
+      ? ((gem.style.width = Math.random() * 300 + 1 + "px"),
+        (gem.style.height = Math.random() * 300 + 1 + "px"))
+      : null;
+    segment.appendChild(gem);
+  }
+}
+
 //segment and particle generator
-segmentsAmount.addEventListener("input", function () {
-  const segmentCount = this.value;
+segmentsAmountInput.addEventListener("input", function () {
+  segmentCount = this.value;
+  createSegments(segmentCount);
+});
+
+function createSegments() {
   kaleidoscope.innerHTML = "";
+  mobileNotification.style.visibility = "hidden";
 
   const angle = 360 / segmentCount;
 
-  const widthLookUp = [
-    0, 100, 100, 174, 100, 73, 58, 48.62, 41.25, 36.66, 32.5, 29.5, 26.6,
-  ];
-
   for (let i = 0; i < segmentCount; i++) {
-    const segment = document.createElement("div");
-    segment.classList.add("segment");
-    segment.style.transform = `rotate(${angle * i}deg)`;
-    segment.style.clipPath = `polygon(50% 0%, 0% 100%, 100% 100%)`;
-    segment.style.backgroundColor =
-      "#" + Math.floor(Math.random() * 16777215).toString(16);
-    segment.style.width = `${widthLookUp[segmentCount]}%`;
-    kaleidoscope.appendChild(segment);
-
-    // particle count control
-    particleCount.addEventListener("input", function () {
-      const gemsAmount = this.value;
-      segment.innerHTML = "";
-      pcInd.innerText = gemsAmount;
-
-      //add particles
-      const gemBox = document.createElement("div");
-
-      for (let i = 0; i < gemsAmount; i++) {
-        const gem = document.createElement("div");
-        gem.classList.add("gem");
-        gem.style.borderColor =
-          "#" + Math.floor(Math.random() * 16777215).toString(16);
-        gem.style.left = Math.random() * segment.clientWidth + "px";
-        gem.style.top = Math.random() * segment.clientHeight + "px";
-        gemBox.appendChild(gem);
-      }
-      segment.appendChild(gemBox);
-    });
+    cloneSegment = segment.cloneNode(true);
+    cloneSegment.style.transform = `rotate(${angle * i}deg)`;
+    cloneSegment.style.width = `${widthLookUp[segmentCount]}%`;
+    kaleidoscope.appendChild(cloneSegment);
   }
 
-  saInd.innerText = segmentCount;
-});
+  segmentIndicator.innerText = segmentCount;
+}
